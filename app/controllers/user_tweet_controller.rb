228d4590f1f19927fa63ad37f16d05f1b1af73b1
Marino <mtejedor85@gmail.com>
2016-04-15 15:08:21 +0200

    Primer commit con la base

---
+++ app/controllers/user_tweet_controller.rb
@@ -0,0 +1,16 @@
+class UserTweetController < ApplicationController
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def read
+  end
+
+  def delete
+  end
+end
---
+++ app/helpers/user_tweet_helper.rb
@@ -0,0 +1,2 @@
+module UserTweetHelper
+end
